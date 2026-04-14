32f8a080187fd9602b5b4f5be650f8b03b534a1e
hypernova-developer <mhmmdeymngrbz@gmail.com>
2026-04-14 23:36:41 +0300

    .

--- Lesson2-Pong_Game/main.cpp
+++ Lesson2-Pong_Game/main.cpp
@@ -26,7 +26,7 @@ public:
         y += speedY;
 
         // What if they touch to the edge? These: 
-        
+
         if (y + radius >= GetScreenHeight() || y - radius <= 0)
         {
             speedY *= -1;
@@ -38,7 +38,67 @@ public:
     }
 };
 
+class Paddle
+{
+protected:
+    void LimitMovement()
+    {
+        if (y <= 0)
+        {
+            y = 0;
+        }
+        if (y + height >= GetScreenHeight())
+        {
+            y = GetScreenHeight() - height;
+        }
+    }
+
+public:
+    float x;
+    float y;
+    float width;
+    float height;
+    int speed;
+
+    void Draw()
+    {
+        DrawRectangle(x, y, width, height, WHITE);
+    }
+
+    void Update()
+    {
+        if (IsKeyDown(KEY_UP))
+        {
+            y = y - speed;
+        }
+        if (IsKeyDown(KEY_DOWN))
+        {
+            y = y + speed;
+        }
+        LimitMovement();
+    }
+};
+
+class CpuPaddle: public Paddle
+{
+public:
+    void Update(int ball_y)
+    {
+        if (y + height / 2 > ball_y)
+        {
+            y = y - speed;
+        }
+        if (y + height / 2 <= ball_y)
+        {
+            y = y + speed;
+        }
+        LimitMovement();
+    }
+};
+
 Ball ball;
+Paddle player;
+CpuPaddle cpu;
 
 void sleepForMilliSeconds(int ms)
 {
@@ -53,14 +113,32 @@ int main()
     const int screenHeight = 800;
     const int targetedFPS = 60;
 
-    InitWindow(screenWidth, screenHeight, "My Pong Game-5 =]");
+    InitWindow(screenWidth, screenHeight, "My Pong Game-7 =]");
     SetTargetFPS(targetedFPS);
+
+    // Initializing the Ball...
 
     ball.radius = 20;
     ball.x = screenWidth / 2;
     ball.y = screenHeight / 2;
     ball.speedX = 7;
     ball.speedY = 7;
+
+    // Initializing the Player Paddle...
+    
+    player.width = 25;
+    player.height = 120;
+    player.x = screenWidth - player.width - 10;
+    player.y = screenHeight / 2 - player.height / 2;
+    player.speed = 6;
+
+    // Initializing the CPU Paddle
+
+    cpu.height = 120;
+    cpu.width = 25;
+    cpu.x = 10;
+    cpu.y = screenHeight / 2 - cpu.height / 2;
+    cpu.speed = 6;
 
     // Main Code of the Game
     // It wil continue over and over if the windows shall not close.
@@ -70,14 +148,16 @@ int main()
 
         // Updating the Elements
         ball.Update();
+        player.Update();
+        cpu.Update(ball.y);
 
         // Things that will be drewn should stay here until EndDrawing();
         ball.Draw();
+        cpu.Draw();
 
         // Drawing the Paddles as Rectangles...
         ClearBackground(BLACK);
-        DrawRectangle(10, screenHeight / 2 - 60, 25, 120, WHITE);
-        DrawRectangle(screenWidth - 35, screenHeight / 2 - 60, 25, 120, WHITE);
+        player.Draw();
         
         // Drawing the Central Line as a Line
         DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE);
